8880022700422331ab78bfc580c8b5a64fdabe52
Ingo Berg <github_contact@beltoforion.de>
2020-06-30 02:35:21 +0200

    k

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sentio_prober_control",
-    version="0.1.0",
+    version="0.1.1",
     author="Ingo Berg",
     author_email="ingo.berg@atv-systems.de",
     description="MPI Sentio Prober Control - Python Bindings",
