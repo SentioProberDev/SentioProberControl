1e5b79cca56bfa60596094c69cbabdbedd2b234f
Ingo Berg <github_contact@beltoforion.de>
2021-07-06 12:19:57 +0200

    comments added

--- sentio_prober_control/Test/tip_detection.py
+++ sentio_prober_control/Test/tip_detection.py
@@ -28,27 +28,97 @@ def main():
         check_preconditions(prober)
 
         prober.select_module(Module.Vision)
+
+        ###########################################################################################
+        #
+        # Scope Camera
+        #
+        ###########################################################################################
+
         prober.vision.switch_camera(CameraMountPoint.Scope)
         comm.send("*DEMO_IMAGE scope, scope_celadon_10x.jpg")
+
+        #
+        # Keypoint Detector:
+        # A generic detector originally designed for automatic stitching of panoramic images.
+        # Will snap to Edges and high contrast areas.
+        #
         prober.vision.detect_probetips(CameraMountPoint.Scope, ProbeTipDetector.Keypoint)
         time.sleep(2)
         prober.vision.remove_probetip_marker()
+
+        #
+        # Probe tip Detector:
+        # A haar cascade based detector trained on tip images seen from above.
+        #
+
+        prober.vision.detect_probetips(CameraMountPoint.Scope, ProbeTipDetector.ProbeTip)
+        time.sleep(2)
+        prober.vision.remove_probetip_marker()
+
+        #
+        # Experimental:  Detector for titan heads in north orientation:
+        # A haar cascade based detector trained on images of titan heads (in north position only!)
+        #
 
         comm.send("*DEMO_IMAGE scope, titan_heads_5x.jpg")
         prober.vision.detect_probetips(CameraMountPoint.Scope, ProbeTipDetector.TitanHead)
         time.sleep(2)
         prober.vision.remove_probetip_marker()
 
+        #
+        # Experimental:  Detector for titan tips:
+        # A haar cascade based detector trained on images of titan tips
+        #
+
+        prober.vision.detect_probetips(CameraMountPoint.Scope, ProbeTipDetector.TitanTip)
+        time.sleep(2)
+        prober.vision.remove_probetip_marker()
+
+        ###########################################################################################
+        #
+        # Chuck Camera
+        #
+        ###########################################################################################
+
         prober.vision.switch_camera(CameraMountPoint.Chuck)
         comm.send("*DEMO_IMAGE chuck, probe_tips_bottom.jpg")
+
+        #
+        # Keypoint Detector:
+        # A generic detector originally designed for automatic stitching of panoramic images.
+        # Will snap to Edges and high contrast areas.
+        #
+
+        prober.vision.detect_probetips(CameraMountPoint.Chuck, ProbeTipDetector.Keypoint)
+        time.sleep(2)
+        prober.vision.remove_probetip_marker()
+
+        #
+        # Probe tips from below:
+        # A haar cascade based detector trained on images of tips seen and illuminated from below
+        #
+
         prober.vision.detect_probetips(CameraMountPoint.Chuck, ProbeTipDetector.ProbeTipFromBelow)
         time.sleep(2)
         prober.vision.remove_probetip_marker()
+
+        #
+        # PyramidTipRingLight Detector:
+        # A haar cascade based detector trained on images with pyramid tips as seen from the chuck camera and
+        # illuminated with the ring light
+        #
 
         comm.send("*DEMO_IMAGE chuck, pyramide_tips_ring.jpg")
         prober.vision.detect_probetips(CameraMountPoint.Chuck, ProbeTipDetector.PyramidTipRingLight)
         time.sleep(2)
         prober.vision.remove_probetip_marker()
+
+        #
+        # PyramidTipSpotLight Detector:
+        # A haar cascade based detector trained on images with pyramid tips as seen from the chuck camera and
+        # illuminated with the spot light
+        #
 
         comm.send("*DEMO_IMAGE chuck, pyramide_tips_spot.jpg")
         prober.vision.detect_probetips(CameraMountPoint.Chuck, ProbeTipDetector.PyramidTipSpotLight)
