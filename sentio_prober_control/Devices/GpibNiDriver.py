6917415f64cfcd626ce4a0be806eaa1bb726780c
Ingo Berg <2202567+beltoforion@users.noreply.github.com>
2021-09-17 12:43:08 +0200

    minor updates

--- sentio_prober_control/Devices/GpibNiDriver.py
+++ sentio_prober_control/Devices/GpibNiDriver.py
@@ -22,9 +22,9 @@ class GpibNiDriver:
         self._gpibThreadIbcnt = gpibDll.ThreadIbcnt
         self._gpibThreadIbcnt.restype = c_int
 
-#        self._gpibIbfind = gpibDll.ibfindW
-#        self._gpibIbfind.restype = c_int
-#        self._gpibIbfind.argtypes = [c_wchar_p]
+        self._gpibIbfind = gpibDll.ibfindW
+        self._gpibIbfind.restype = c_int
+        self._gpibIbfind.argtypes = [c_wchar_p]
 
         self._gpibIbsic = gpibDll.ibsic
         self._gpibIbsic.restype = c_int
@@ -40,11 +40,20 @@ class GpibNiDriver:
 
 
     def connect(self, board_name: str, address: int):
+        # check wether the board exists
+        board_handle = self._gpibIbfind(board_name)
+        if board_handle < 0:
+            raise Exception(f'Board "{board_name}" does not exist!')
+
         # init
         numbers = re.findall('[0-9]+', board_name)
         self._board = int(numbers[0])
 
         self._gpibSendIfc(self._board)
+        ibsta, iberr, ibcnt, ibcntl = self.get_globals()
+
+        if iberr != 0:
+            raise Exception(f'Connect failed: Gpib error code {iberr}')
 
         self._addr = address
         self._max_len = 1000
@@ -61,8 +70,16 @@ class GpibNiDriver:
     def send(self, str):
         self._gpibSendImpl(self._board, self._addr, str.encode('utf-8'), len(str), 1)
 
+        iberr = self._gpibThreadIberr()
+        if iberr != 0:
+            raise Exception(f'Sending "{str}" failed. (GPIB error code {iberr})')
+
     def receive(self):
         byteArr = (c_byte * self._max_len)()
         self._gpibReceiveImpl(self._board, self._addr, byteArr, self._max_len, 0)
         ibsta, iberr, ibcnt, ibcntl = self.get_globals()
+
+        if iberr != 0:
+            raise Exception(f'Receive failed: Gpib error code {iberr}')
+
         return bytearray(byteArr[:ibcntl]).decode("utf-8")
--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sentio-prober-control-SentioProberDev",
-    version="3.5.0",
+    name="sentio-prober-control",
+    version="3.5.1",
     author="Ingo Berg",
     author_email="ingo.berg@atv-systems.de",
     description="MPI Sentio Prober Control - Python Bindings",
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://www.mpi-corporation.com/ast/engineering-probe-systems/mpi-sentio-software-suite/",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Programming Language :: Python :: 3",
         "License :: Other/Proprietary License",
