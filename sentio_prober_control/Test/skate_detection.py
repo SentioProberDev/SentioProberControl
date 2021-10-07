7556cf3db742015ae8708b3e892bbf7e60af8590
Ingo Berg <2202567+beltoforion@users.noreply.github.com>
2021-10-07 15:33:45 +0200

    Revert "update"
    This reverts commit 85184bb5373012dae3dd960705b479bc2475ab00.

--- sentio_prober_control/Test/skate_detection.py
+++ sentio_prober_control/Test/skate_detection.py
@@ -9,34 +9,11 @@ import time
 # required SENTIO Version: 3.6.0 or higher
 #
 
-def check_preconditions(prober: SentioProber):
+def check_preconditions(prober):
     # 1.) Check whether the machine has a motorized chuck.
     if (not prober.vision.has_camera(CameraMountPoint.Scope)):
         raise Exception("This script requires a scope camera in order to run!")
 
-def train_initial_setup(prober: SentioProber):
-        prober.select_module(Module.Vision)
-        prober.vision.switch_camera(CameraMountPoint.Scope)
-        prober.comm.send("*DEMO_IMAGE scope, wizard_skate_detection\step3_cont.jpg")
-
-        resp = prober.send_cmd("vis:sd:find_initial_setup scope, east,west")
-        resp.dump()
-
-def test_offset_loop(prober: SentioProber):
-
-    chuckx, chucky = prober.get_chuck_xy(ChuckSite.Wafer, ChuckXYReference.Zero)
-    print(f"Initial chuck position: {chuckx}, {chucky}")
-
-    try:
-        while True:
-            resp = prober.send_cmd("vis:sd:find_offset scope, true")
-            if not resp.ok:
-                raise Exception("vis:sd:find_offset error!")
-
-            tok = resp.message().split(",")
-            print(f"offset: {float(tok[0])}, {float(tok[1])}")
-    except KeyboardInterrupt:
-        pass
 
 def main():
     try:
@@ -47,17 +24,11 @@ def main():
 
         check_preconditions(prober)
 
-        #
-        # Training of the setup (probes in pad contact, models need to be trained)
-        #
-
-        # train_initial_setup(prober)
-
-#        prober.open_project("C:\\ProgramData\\MPI Corporation\\SENTIO\\projects\\test_skate_detection", True)
-#        prober.move_chuck_z(ChuckZReference.Contact, 0)
-#        prober.move_scope_z(ScopeZReference.Zero, -135624.2)
-
-        test_offset_loop(prober)
+        prober.select_module(Module.Vision)
+        prober.vision.switch_camera(CameraMountPoint.Scope)
+        comm.send("*DEMO_IMAGE scope, wizard_skate_detection\step3_cont.jpg")
+        resp = prober.send_cmd("vis:sd:find_initial_setup scope, east,west")
+        resp.dump()
 
     except Exception as e:
         print("\n#### Error ##################################")
