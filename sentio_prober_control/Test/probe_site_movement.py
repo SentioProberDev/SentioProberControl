9e990117d61bc5f57f17b62c545842df2b0beccb
Hoca Chen <hocachen@gmail.com>
2021-11-08 10:42:01 +0800

    modify the probe_site_movement delete the file of "SENTIO_ProbeSiteMovement"

--- sentio_prober_control/Test/probe_site_movement.py
+++ sentio_prober_control/Test/probe_site_movement.py
@@ -27,13 +27,13 @@ def main():
         3: 100,}
 
     for i in range(1, len(subsiteListEastX)+1):
-        prober.move_probe_xy(ProbeSentio.East, ReferenceXy.Home, subsiteListEastX[i],  subsiteListEastY[i])
-        prober.move_probe_xy(ProbeSentio.West, ReferenceXy.Home, subsiteListWestX[i],  subsiteListWestY[i])
+        prober.probe.move_probe_xy(ProbeSentio.East, ProbeXYReference.Home, subsiteListEastX[i],  subsiteListEastY[i])
+        prober.probe.move_probe_xy(ProbeSentio.West, ProbeXYReference.Home, subsiteListWestX[i],  subsiteListWestY[i])
 
 
 if __name__ == "__main__":
     try:
         main()
     except Exception as e:
-        print("\n#### Error ##################################")
+        print("\n#### Error ####")
         print("{0}".format(e))
