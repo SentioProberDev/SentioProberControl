c9f41e547994ee0a024ad6b6465c607a450021ad
Ingo Berg <ingo.berg@atv-systems.de>
2023-08-30 14:31:58 +0200

    toml file updated
    added pyvisa and pdoc as build time dependencies
    added pyvisa as runtime dependency

--- sentio_prober_control/Communication/__init__.py
+++ sentio_prober_control/Communication/__init__.py
@@ -1,2 +1,7 @@
+""" Communication package for the Sentio Prober Control software.
+
+    This package contains classes for communication with the MPI probe station with
+    different protocols: TCP/IP, Gpib or VISA.
+"""
 print(f'Initializing package {__name__}')
 
