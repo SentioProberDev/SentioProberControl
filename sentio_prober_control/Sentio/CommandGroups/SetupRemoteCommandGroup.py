ac1bd6036e7b31321af6613aa5b2a4d3f8fb1c74
Ingo Berg <ingo.berg@atv-systems.de>
2025-05-08 11:47:37 +0200

    Made Python api consistent with remote command api
    - created two new command groups for setup in order to match hierarchy of the remote command
    - remote return values from "remote" command group functions

---
+++ sentio_prober_control/Sentio/CommandGroups/SetupRemoteCommandGroup.py
@@ -0,0 +1,44 @@
+from sentio_prober_control.Sentio.Response import Response
+from sentio_prober_control.Sentio.CommandGroups.CommandGroupBase import CommandGroupBase
+
+class SetupRemoteCommandGroup(CommandGroupBase):
+    """ This command group bundles functions setting up the behavior of SENTIO in remote mode. """
+    def __init__(self, comm) -> None:
+        super().__init__(comm)
+
+    def light_off_at_contact(self, status: bool) -> None:
+        """Defines whether light is switched off at contact height in remote mode.
+
+        Args:
+            status (BooleanStatus): True/False, ON/OFF as defined in Enum.
+
+        Returns:
+            None
+        """
+        self.comm.send(f"setup:remote:light_off_at_contact {status}")
+        Response.check_resp(self.comm.read_line())
+                
+    def light_on_at_separation(self, status: bool) -> None:
+        """Defines whether light is switched on at separation height in remote mode.
+
+        Args:
+            status (BooleanStatus): True/False, ON/OFF as defined in Enum.
+
+        Returns:
+            None
+        """
+        self.comm.send(f"setup:remote:light_on_at_separation {status}")
+        Response.check_resp(self.comm.read_line())
+        
+    def scope_follow_off(self, status: bool) -> None:
+        """Defines whether scope follow mode is switched off in remote mode.
+
+        Args:
+            status (BooleanStatus): True/False, ON/OFF as defined in Enum.
+                Note: ON disables scope follow.
+
+        Returns:
+            Response: Response object for result checking.
+        """
+        self.comm.send(f"setup:remote:scope_follow_off {status}")
+        Response.check_resp(self.comm.read_line())
