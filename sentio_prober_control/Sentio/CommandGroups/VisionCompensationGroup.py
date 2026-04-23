faf65eb2e50b0ac095b7bee178dacd1eb34ab3f4
ingo <berg@atv-systems.de>
2026-04-23 08:47:08 +0200

    fix for #49

--- sentio_prober_control/Sentio/CommandGroups/VisionCompensationGroup.py
+++ sentio_prober_control/Sentio/CommandGroups/VisionCompensationGroup.py
@@ -1,11 +1,11 @@
-from typing import Tuple
-
-
 from sentio_prober_control.Sentio.Enumerations import CompensationMode, CompensationType
 from sentio_prober_control.Sentio.Response import Response
 from sentio_prober_control.Sentio.CommandGroups.CommandGroupBase import CommandGroupBase
 
+from typing import Tuple, TYPE_CHECKING
 
+if TYPE_CHECKING:
+    from sentio_prober_control.Sentio.ProberSentio import SentioProber
 
 class VisionCompensationGroup(CommandGroupBase):
     """This command group contains functions for working with x,y and z compensation.
@@ -14,7 +14,7 @@ class VisionCompensationGroup(CommandGroupBase):
     of the vision attribute of the [SentioProber](SentioProber.md) class.
     """
 
-    def __init__(self, prober : 'SentioProber'):
+    def __init__(self, prober : SentioProber) -> None:
         super().__init__(prober)
 
 
@@ -25,10 +25,7 @@ class VisionCompensationGroup(CommandGroupBase):
             This function is obsolete and will be removed in a future release.\
             Use vision.compensation.enable instead"
         """
-        self.comm.send(f"vis:compensation:enable {comp.to_string()}, {enable}")
-        resp = Response.check_resp(self.comm.read_line())
-        tok = resp.message().split(",")
-        return tok[0], tok[1]
+        return self.enable(comp, enable)
 
 
     def enable(self, comp: CompensationMode, enable: bool) -> Tuple[str, str]:
@@ -48,20 +45,36 @@ class VisionCompensationGroup(CommandGroupBase):
         self.comm.send(f"vis:compensation:enable {comp.to_string()}, {enable}")
         resp = Response.check_resp(self.comm.read_line())
         tok = resp.message().split(",")
+        if len(tok) < 2:
+            raise ValueError(f"Expected 2 values in response message, got {len(tok)}: '{resp.message()}'")
+        
         return tok[0], tok[1]
 
-    def start_execute(self, type: CompensationType, mode: CompensationMode) -> Response:
+    def start_execute(self, compensation_type: CompensationType, mode: CompensationMode | None = None, moveToOffset: bool | None = None) -> Response:
         """Start the execution of a compensation.
 
         Wraps Sentios "vis:compensation:start_execute" remote command.
 
+        This function was rewritten to fix #49 (https://github.com/SentioProberDev/SentioProberControl/issues/49). 
+        The two last parameters are optional, moveToOffset parameter was added.
+
         Args:
-            type: The type of compensation to execute.
+            compensation_type: The type of compensation to execute.
             mode: The mode of compensation to execute.
+            moveToOffset: Move to the offset position. Only applicable with type=CompensationType.DieAlign and mode!=CompensationMode.ProbeCard.
 
         Returns:
             A Response object.
         """
 
-        self.comm.send(f"vis:compensation:start_execute {type.to_string()}, {mode.to_string()}")
+        if moveToOffset is not None and mode is None:
+            raise ValueError("moveToOffset cannot be used without a mode.")
+        
+        if mode is not None and moveToOffset is not None:
+            self.comm.send(f"vis:compensation:start_execute {compensation_type.to_string()}, {mode.to_string()}, {moveToOffset}")  
+        elif mode is not None:
+            self.comm.send(f"vis:compensation:start_execute {compensation_type.to_string()}, {mode.to_string()}")
+        else:
+            self.comm.send(f"vis:compensation:start_execute {compensation_type.to_string()}")
+
         return Response.check_resp(self.comm.read_line())
